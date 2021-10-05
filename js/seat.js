4a4a295ad8864d62d85f008228d27dc0a5560b7d
Marathoner-dev <marathoner.dev@gmail.com>
2021-10-05 23:40:35 +0900

    자리 뽑기 사이트 생성

---
+++ js/seat.js
@@ -0,0 +1,8 @@
+const setBtn = document.getElementById('setBtn');
+const rowCnt = document.getElementById('inputRow');
+const colCnt = document.getElementById('inputCol');
+
+setBtn.addEventListener('click', () => {
+  console.log(rowCnt.value, colCnt.value)
+
+});
