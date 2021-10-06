e6e2f41756ad261dd04de6d601b148918dbafdeb
Marathoner-dev <marathoner.dev@gmail.com>
2021-10-07 02:46:34 +0900

    자리표 생성 및 파비콘 추가

--- js/seat.js
+++ js/seat.js
@@ -1,8 +1,22 @@
 const setBtn = document.getElementById('setBtn');
-const rowCnt = document.getElementById('inputRow');
-const colCnt = document.getElementById('inputCol');
+const tableBoxEl = document.getElementById("tablebox");
+const addTable = document.createElement("table");
+const addTr = document.createElement("tr")
+const addTd = document.createElement("td")
+
+function makeTable (col, row){
+  const tableChild = document.getElementById("tablebox").appendChild(document.createElement("table"))
+  for (let i = 0; i < col; i++) {
+    let trEls = tableChild.appendChild(document.createElement('tr'))
+    for (let j = 0; j < row; j++) {
+      trEls.appendChild(document.createElement('td'))
+    }
+  }
+}
 
 setBtn.addEventListener('click', () => {
-  console.log(rowCnt.value, colCnt.value)
-
+  document.getElementById("tablebox").innerHTML = ""
+  let rowCnt = document.getElementById('inputRow').value;
+  let colCnt = document.getElementById('inputCol').value;
+  makeTable(colCnt, rowCnt)
 });
